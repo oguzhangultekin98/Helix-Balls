85bd98e3d6d0f6f511e30ca01eaf0e3687ed9ae5
Oguzhan Gultekin <oguzhangultekin098@gmail.com>
2021-11-13 16:11:11 +0300

    BG-Assets-Stage-Pass-Base

---
+++ Assets/Scripts/Behaviours/StagePassBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Passables.Entities;
+using System.Linq;
+
+namespace Assets.Scripts.Behaviours
+{
+    public class StagePassBehaviour : MonoBehaviour
+    {
+        
+        List<StageEntrance> _stages;
+        private int _currentStageIndex; //NeedToKnow How many balls we have
+        private void Awake()
+        {
+            _stages = GameObject.FindObjectsOfType<StageEntrance>()
+                .OrderBy(s => -s.transform.position.z).ToList();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("Z");
+                for (int i = 0; i < _stages.Count; i++)
+                {
+                    Debug.Log(_stages[i].BallCountNeeded);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Passables/Entities/StageEntrance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Passables.Entities
+{
+    public class StageEntrance : PassableBase
+    {
+        public float TopYPoint { get; private set; }
+
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+
+        public float TopPoint => TopYPoint;
+
+        public override void OnHitPassable()
+        {
+            //Holderı bul sonra eğer geçebilirse havada kalmalarını engelle
+        }
+
+    }
+}
