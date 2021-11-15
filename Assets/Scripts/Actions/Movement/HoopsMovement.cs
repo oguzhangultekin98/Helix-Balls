244b7881d1253f8f668df6a55f9448feccdf2491
Oguzhan Gultekin <oguzhangultekin098@gmail.com>
2021-11-15 09:46:10 +0300

    Hoop-Rotation-Fix

--- Assets/Scripts/Actions/Movement/HoopsMovement.cs
+++ Assets/Scripts/Actions/Movement/HoopsMovement.cs
@@ -3,59 +3,43 @@ using UnityEngine;
 
 namespace Assets.Scripts.Actions.Movement
 {
-    
     public class HoopsMovement : MovementBase
     {
-      private Joystick _joystick;
-        private GravityHolder _gravityHolder = new GravityHolder();
+        private Joystick _joystick;
 
-        protected Vector3 impact;
-        public CharacterController MovementComponent { get; private set; }
-        public bool Activated { get; private set; }
-
-        [SerializeField] protected TransformInterpolator transformInterpolater;
+        //[SerializeField] protected TransformInterpolator transformInterpolater;
 
         private void Awake()
         {
             _joystick = FindObjectOfType<Joystick>(true);
         }
-        private void Start()
-        {
-            transformInterpolater.oldQuaternion = Quaternion.identity;
-        }
+
         private void Update()
         {
-            #region Delete
-            if (Input.GetKey(KeyCode.A))
-            {
-                this.transform.Rotate(Vector3.up, 0.5f * Time.deltaTime);
-            }
-            Debug.Log("Hor" + _joystick.Horizontal);
-
-            #endregion
-
             GetInput();
-
-
         }
 
         private void GetInput()
         {
             var rotData = new Vector3(0, _joystick.Horizontal, 0);
-            if (Mathf.Abs(rotData.y)>0.01f)
-                Rotate(rotData);
+            if (rotData.y > 0.01f)
+                Rotate(Vector3.down);
+            else if (rotData.y < -0.01f)
+                Rotate(Vector3.up);
         }
 
-        public void Rotate(Vector3 data)
+        private void Rotate(Vector3 data)
         {
-            var lerp = Mathf.Lerp(transform.rotation.y, data.y, Time.deltaTime * 10f);
-            
+            /* Shaking problems 
             Quaternion targetRotation = Quaternion.Lerp(transformInterpolater.oldQuaternion,
-                Quaternion.Euler(data*360),
+                Quaternion.Euler(data*speedMultiplier*Time.deltaTime)*transformInterpolater.oldQuaternion,
                 transformInterpolater.quaternionLerpCoefficient);
-
+            
             transformInterpolater.oldQuaternion = transform.rotation;
             transform.rotation = targetRotation;
+        */
+            transform.Rotate(data*Time.deltaTime*speedMultiplier);
+
         }
 
 
